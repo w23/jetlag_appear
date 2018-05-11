9231815f6687e46b4796db6fc88b1ca6b82106b3
Ivan Avdeev <me@w23.ru>
2018-05-11 16:33:12 +0700

    yo29: minification work (4273 fast, 4184 slow)

--- yo29_4k/main.shader
+++ yo29_4k/main.shader
@@ -1,6 +1,9 @@
 //#version 130
 //uniform int F[1];
-float t = $(float time);
+//uniform
+vec3 F = vec3($(vec2 resolution), $(float time));
+float t = F.z;
+
 uniform sampler2D S;
 vec3 E = vec3(0.,.01,1.);
 vec4 noise24(vec2 v) { return texture2D(S, (v + .5)/textureSize(S,0)); }
@@ -33,7 +36,7 @@ float noise31(vec3 v) {
 }*/
 
 float noise31(vec3 v) {
-	return mix(noise24(v.xz).x, noise24(v.yx).y, .5);// fract(v.z));
+	return (noise24(v.xz).x + noise24(v.yx).y) * .5;
 }
 
 bool clouds = true;
@@ -41,31 +44,22 @@ vec2 densitiesRM(vec3 p) {
 	float h = max(0., length(p - C) - R0);
 	vec2 retRM = vec2(exp(-h/Hr), exp(-h/Hm) * 8.);
 
-	if (clouds) {
-		/*
-		float r = length(p.xz);
-		if (r < 100. && p.y < 500.) {
-			retRM.y += 6e3 * smoothstep(.35, .55, fnoise(p*5e-2-vec3(0.,t*2.,0.))) * step(r, 8. + .3 * (p.y - 200.));
-		}
-		*/
-
-		if (low < h && h < hi) {
-			vec3 v = 15e-4 * (p + t * vec3(-90., 0., 80.));
-			retRM.y +=
-				250. *
-				//step(length(p), 50000.) *
-				//smoothstep(48., 0., t - 160.) *
-				//smoothstep(28., 23., v.z) *
-				step(v.z, 38.) *
-				smoothstep(low, low + 1e2, h) *
-				smoothstep(hi, hi - 1e3, h) *
-				smoothstep(.5, .55,
-					.75 * noise31(v)
-					+ .125 * noise31(v*4.3 + max(t, 144.))
-					+ .0625 * noise31(v*9.9)
-					+ .0625 * noise31(v*17.8)-.1
-				);
-		}
+	if (clouds && low < h && h < hi) {
+		vec3 v = 15e-4 * (p + t * vec3(-90., 0., 80.));
+		retRM.y +=
+			250. *
+			//step(length(p), 50000.) *
+			//smoothstep(48., 0., t - 160.) *
+			//smoothstep(28., 23., v.z) *
+			step(v.z, 38.) *
+			smoothstep(low, low + 1e2, h) *
+			smoothstep(hi, hi - 1e3, h) *
+			smoothstep(.5, .55,
+				.75 * noise31(v)
+				+ .125 * noise31(v*4.3 + max(t, 144.))
+				+ .0625 * noise31(v*9.9)
+				+ .0625 * noise31(v*17.8)-.1
+			);
 	}
 
 	return retRM;
@@ -87,7 +81,6 @@ vec4 random;
 vec2 scatterDirectImpl(vec3 o, vec3 d, float L, float steps) {
 	vec2 depthRMs = vec2(0.);
 	L /= steps; d *= L;
-	//o += d * noise24(o.xz*1e3 + vec2(t*1e4)).x;
 	for (float i = random.w * .5; i < steps; ++i)
 		depthRMs += densitiesRM(o + d * i);
 	return depthRMs * L;
@@ -95,7 +88,7 @@ vec2 scatterDirectImpl(vec3 o, vec3 d, float L, float steps) {
 
 vec3 Lin(vec3 o, vec3 d, float L) {
 	vec2 depthRMs = scatterDirectImpl(o, d, L, 32.);
-	return exp(-(bR * depthRMs.x + bMe * depthRMs.y));
+	return exp(- bR * depthRMs.x - bMe * depthRMs.y);
 }
 
 vec2 totalDepthRM;
@@ -125,10 +118,9 @@ void scatterImpl(vec3 o, vec3 d, float L, float steps) {
 		*/
 
 		vec2 depthRMsum = totalDepthRM;
-		float l = escape(p, sundir, R0 + hi);
+		float l = max(0., escape(p, sundir, R0 + hi));
 		if (l > 0.)
 			depthRMsum += scatterDirectImpl(p, sundir, l, 16.);
-		else l = 0.;
 		depthRMsum += scatterDirectImpl(p + sundir * l, sundir, escape(p, sundir, Ra), 4.);
 
 		//vec2 depthRMsum = totalDepthRM + scatterDirectImpl(p, sundir, escape(p, sundir, Ra), 32.);
@@ -139,7 +131,7 @@ void scatterImpl(vec3 o, vec3 d, float L, float steps) {
 		*/
 
 
-		vec3 A = exp(-(bR * depthRMsum.x + bMe * depthRMsum.y));
+		vec3 A = exp(-bR * depthRMsum.x - bMe * depthRMsum.y);
 		LiR += A * dRM.x;
 		LiM += A * dRM.y;
 	}
@@ -176,7 +168,7 @@ vec3 scatter(vec3 o, vec3 d, float L, vec3 Lo) {
 	}
 
 	float mu = dot(d, sundir);
-	return Lo * exp(-(bR * totalDepthRM.x + bMe * totalDepthRM.y))
+	return Lo * exp(-bR * totalDepthRM.x - bMe * totalDepthRM.y)
 		+ I * (1. + mu * mu) * (
 			LiR * bR * .0596831 +
 			LiM * bMs * .1193662 * (1. - g2) / ((2. + g2) * pow(1. + g2 - 2. * g * mu, 1.5)));
@@ -193,21 +185,9 @@ vec2 rep2(vec2 p, vec2 s) { return mod(p, s) - s*.5; }
 
 float mindex = 0.;
 vec2 mparam = vec2(0.);
-float dbg = 0.;
-
 const float guard = 4.;
 vec4 rnd, rnd2;
-float cellDist(vec4 cp, vec2 cell) {
-	rnd = noise24(cell);
-	rnd2 = noise24(cell/8.);
-	float height = 3. * floor(2. + rnd.w * 4. + 20. * smoothstep(.8,.99, rnd.w) * smoothstep(.6, .9, rnd2.w));
 
-	return min(
-		min(
-			max(vmax3(cp.xyz + rnd.xyz * 4.), cp.w - height),
-			max(vmax3(cp.xyz + rnd.zxw * 7.), cp.w - height - 3. * floor(rnd.w * 4.))),
-		max(vmax3(cp.yzx + 10. * rnd.xyz), cp.w - height + 3. * floor(rnd.y * 2.)));
-}
 
 float world(vec3 p) {
 	vec2 road_normal = normalize(vec2(-1.,2.));
@@ -223,9 +203,19 @@ float world(vec3 p) {
 
 	vec2 cell = floor(p.xz / 30.);
 	p.xz = rep2(p.xz, vec2(30.));
+	rnd = noise24(cell);
+	rnd2 = noise24(cell/8.);
+
 	vec3 cell_border = vec3(abs(p.xz)-15., highway);
+	vec3 cp = vec3(cell_border + guard);
 	float b = min(vmax2(cell_border.xy), -cell_border.z);
-	float cd = cellDist(vec4(cell_border + guard, p.y), cell);
+	float height = 3. * floor(2. + rnd.w * 4. + 20. * smoothstep(.8,.99, rnd.w) * smoothstep(.6, .9, rnd2.w));
+	float cd = min(
+		min(
+			max(vmax3(cp.xyz + rnd.xyz * 4.), p.y - height),
+			max(vmax3(cp.xyz + rnd.zxw * 7.), p.y - height - 3. * floor(rnd.w * 4.))),
+		max(vmax3(cp.yzx + 10. * rnd.xyz), p.y - height + 3. * floor(rnd.y * 2.)));
+
 	mparam =
 		mix(
 			vec2(
@@ -241,6 +231,7 @@ float world(vec3 p) {
 		mindex = 0.;
 		mparam = cell_border.xy;
 	}
+	//return max(p.y-90., min(d, -b + guard));
 	return max(p.y-90., min(d, -b + guard));
 }
 
@@ -251,13 +242,12 @@ void march(float maxL) {
 		P = O + D * L;
 		float dd = world(P);
 		L += dd;
-		if (dd < .0003 * L  || L > maxL) return;
+		if (dd < L / F.x  || L > maxL) return;
 	}
 	L = maxL;
 }
 
-float m_kd = .3;
-float m_shine;
+float m_kd, m_shine;
 float dirlight(vec3 dir) {
 	return mix(
 		max(0., dot(N, dir)) / 3.,
@@ -272,9 +262,8 @@ float poslight(vec3 pos) {
 
 void main() {
 	random = noise24(gl_FragCoord.xy + t * 1e4);
-	vec2 uv = (gl_FragCoord.xy + random.xy)/$(vec2 resolution)* 2. - 1.; uv.x *= $(vec2 resolution).x / $(vec2 resolution).y;
 
-	//if (gl_FragCoord.y < 10.) { gl_FragColor = vec4(vec3(step(gl_FragCoord.x / $(vec2 resolution).x, t / 232.)), 1.); return; }
+	//if (gl_FragCoord.y < 10.) { gl_FragColor = vec4(vec3(step(gl_FragCoord.x / F.x, t / 232.)), 1.); return; }
 
 	vec3 at = vec3(0.);
 	O = vec3(
@@ -317,8 +306,7 @@ void main() {
 	//O = $(vec3 cam_pos) * 3.; D = -normalize($(vec3 cam_dir));
 
 	vec3 x = normalize(cross(E.xzx, D));
-	D = mat3(x, normalize(cross(D, x)), D) * normalize(vec3(uv, -2.)); // + noise24(uv+t*1e3).xy / $(vec2 resolution), -2.));
-	//D = mat3(x, normalize(cross(D, x)), D) * normalize(vec3(uv + noise24(uv+t*1e3).xy / $(vec2 resolution), -2.));
+	D = mat3(x, normalize(cross(D, x)), D) * normalize(vec3((gl_FragCoord.xy + random.xy)/F.xy * 2. - 1., -2.) * vec3(F.x / F.y, 1., 1.));
 
 	vec3 color = vec3(0.);
 
--- yo29_4k/main.shader.glsl
+++ yo29_4k/main.shader.glsl
@@ -1,13 +1,16 @@
 //#version 130
 //uniform int F[1];
-float t = F[0];
+//uniform
+vec3 F = vec3(vec2(0.000,0.000), F[0]);
+float t = F.z;
+
 uniform sampler2D S;
 vec3 E = vec3(0.,.01,1.);
 vec4 noise24(vec2 v) { return texture2D(S, (v + .5)/textureSize(S,0)); }
 
 const float R0 = 6360e3;
 const float Ra = 6380e3;
-const float low = 2e3, hi = 4e3, border = 1e3;
+const float low = 1e3, hi = 25e2, border = 5e2;
 const float g = .76;
 const float g2 = g * g;
 const float Hr = 8e3;
@@ -19,7 +22,7 @@ const vec3 bR = vec3(58e-7, 135e-7, 331e-7);
 const vec3 bMs = vec3(2e-5);
 const vec3 bMe = bMs * 1.1;
 
-vec3 sundir = vec3(1.,.001,1.);
+vec3 sundir = vec3(1.,.01,1.);
 
 /*
 float noise31(vec3 v) {
@@ -33,45 +36,30 @@ float noise31(vec3 v) {
 }*/
 
 float noise31(vec3 v) {
-	return mix(noise24(v.xz).x, noise24(v.yx).y, .5);// fract(v.z));
-}
-
-float fnoise(vec3 v) {
-	return
-		.75 * noise31(v)
-		+ .125 * noise31(v*4.3)
-		//+ .0625 * noise31(v*9.9)
-		//+ .0625 * noise31(v*17.8)
-		;
-}
-
-float cloud(vec3 p) {
-	float cld = fnoise(p*1e-3);
-	cld = smoothstep(.49, .5, cld);
-	return cld;
+	return (noise24(v.xz).x + noise24(v.yx).y) * .5;
 }
 
 bool clouds = true;
 vec2 densitiesRM(vec3 p) {
 	float h = max(0., length(p - C) - R0);
-	vec2 retRM = vec2(exp(-h/Hr), exp(-h/Hm) * 4.);
+	vec2 retRM = vec2(exp(-h/Hr), exp(-h/Hm) * 8.);
 
-	if (clouds) {
-		/*
-		float r = length(p.xz);
-		if (r < 100. && p.y < 500.) {
-			retRM.y += 6e3 * smoothstep(.35, .55, fnoise(p*5e-2-vec3(0.,t*2.,0.))) * step(r, 8. + .3 * (p.y - 200.));
-		}
-		*/
-
-		if (low < h && h < hi) {
-			retRM.y +=
-				50. *
-				step(length(p), 50000.) *
-				smoothstep(low, low + border, h) *
-				smoothstep(hi, hi - border, h) *
-				cloud(p+vec3(0., 0.,t*80.));
-		}
+	if (clouds && low < h && h < hi) {
+		vec3 v = 15e-4 * (p + t * vec3(-90., 0., 80.));
+		retRM.y +=
+			250. *
+			//step(length(p), 50000.) *
+			//smoothstep(48., 0., t - 160.) *
+			//smoothstep(28., 23., v.z) *
+			step(v.z, 38.) *
+			smoothstep(low, low + 1e2, h) *
+			smoothstep(hi, hi - 1e3, h) *
+			smoothstep(.5, .55,
+				.75 * noise31(v)
+				+ .125 * noise31(v*4.3 + max(t, 144.))
+				+ .0625 * noise31(v*9.9)
+				+ .0625 * noise31(v*17.8)-.1
+			);
 	}
 
 	return retRM;
@@ -88,27 +76,28 @@ float escape(vec3 p, vec3 d, float R) {
 	return (t1 >= 0.) ? t1 : t2;
 }
 
+vec4 random;
+
 vec2 scatterDirectImpl(vec3 o, vec3 d, float L, float steps) {
 	vec2 depthRMs = vec2(0.);
 	L /= steps; d *= L;
-	//o += d * noise24(o.xz*1e3 + vec2(t*1e4)).x;
-	for (float i = 0.; i < steps; ++i)
-		//depthRMs += densitiesRM(o + d * (i + noise24(vec2(i,i)).x));
+	for (float i = random.w * .5; i < steps; ++i)
 		depthRMs += densitiesRM(o + d * i);
 	return depthRMs * L;
 }
 
 vec3 Lin(vec3 o, vec3 d, float L) {
 	vec2 depthRMs = scatterDirectImpl(o, d, L, 32.);
-	return exp(-(bR * depthRMs.x + bMe * depthRMs.y));
+	return exp(- bR * depthRMs.x - bMe * depthRMs.y);
 }
 
-void scatterImpl(vec3 o, vec3 d, float L, float steps, inout vec2 totalDepthRM, inout vec3 LiR, inout vec3 LiM) {
+vec2 totalDepthRM;
+vec3 LiR, LiM;
+
+void scatterImpl(vec3 o, vec3 d, float L, float steps) {
 	L /= steps; d *= L;
-	for (float i = 0.; i < steps; ++i) {
+	for (float i = random.z; i < steps; ++i) {
 		vec3 p = o + d * i;
-		//p += d * noise24(p.xz).w;
-		//vec3 p = o + d * i;(i + noise24(vec2(i,i)).x)
 		vec2 dRM = densitiesRM(p) * L;
 		totalDepthRM += dRM;
 
@@ -129,10 +118,9 @@ void scatterImpl(vec3 o, vec3 d, float L, float steps, inout vec2 totalDepthRM, 
 		*/
 
 		vec2 depthRMsum = totalDepthRM;
-		float l = escape(p, sundir, R0 + hi);
+		float l = max(0., escape(p, sundir, R0 + hi));
 		if (l > 0.)
 			depthRMsum += scatterDirectImpl(p, sundir, l, 16.);
-		else l = 0.;
 		depthRMsum += scatterDirectImpl(p + sundir * l, sundir, escape(p, sundir, Ra), 4.);
 
 		//vec2 depthRMsum = totalDepthRM + scatterDirectImpl(p, sundir, escape(p, sundir, Ra), 32.);
@@ -143,15 +131,15 @@ void scatterImpl(vec3 o, vec3 d, float L, float steps, inout vec2 totalDepthRM, 
 		*/
 
 
-		vec3 A = exp(-(bR * depthRMsum.x + bMe * depthRMsum.y));
+		vec3 A = exp(-bR * depthRMsum.x - bMe * depthRMsum.y);
 		LiR += A * dRM.x;
 		LiM += A * dRM.y;
 	}
 }
 
 vec3 scatter(vec3 o, vec3 d, float L, vec3 Lo) {
-	vec2 totalDepthRM = vec2(0.);
-	vec3 LiR = vec3(0.), LiM = vec3(0.);
+	totalDepthRM = vec2(0.);
+	LiR = LiM = vec3(0.);
 
 	/*
 	float det_dist = 1e4;
@@ -161,20 +149,26 @@ vec3 scatter(vec3 o, vec3 d, float L, vec3 Lo) {
 		scatterImpl(o + d * det_dist, d, left, 32., totalDepthRM, LiR, LiM);
 	} else
 		scatterImpl(o, d, L, 32., totalDepthRM, LiR, LiM);
-		*/
+	*/
 
+	//float l = 10e3;//escape(o, d, R0 + low);//, l2 = escape(o, d, R0 + hi);
 	float l = escape(o, d, R0 + low);//, l2 = escape(o, d, R0 + hi);
 	if (L < l)
-		scatterImpl(o, d, L, 16., totalDepthRM, LiR, LiM);
+		scatterImpl(o, d, L, 16.);
 	else {
+		/*
+		scatterImpl(o, d, l, 32.);
+		scatterImpl(o+d*l, d, L-l, 8.);
+		*/
+
 		float l2 = escape(o, d, R0 + hi);
-		scatterImpl(o, d, l, 32., totalDepthRM, LiR, LiM);
-		scatterImpl(o+d*l, d, l2-l, 32., totalDepthRM, LiR, LiM);
-		scatterImpl(o+d*l2, d, L-l2, 8., totalDepthRM, LiR, LiM);
+		scatterImpl(o, d, l, 16.);
+		scatterImpl(o+d*l, d, l2-l, 40.);
+		scatterImpl(o+d*l2, d, L-l2, 8.);
 	}
 
 	float mu = dot(d, sundir);
-	return Lo * exp(-(bR * totalDepthRM.x + bMe * totalDepthRM.y))
+	return Lo * exp(-bR * totalDepthRM.x - bMe * totalDepthRM.y)
 		+ I * (1. + mu * mu) * (
 			LiR * bR * .0596831 +
 			LiM * bMs * .1193662 * (1. - g2) / ((2. + g2) * pow(1. + g2 - 2. * g * mu, 1.5)));
@@ -191,32 +185,37 @@ vec2 rep2(vec2 p, vec2 s) { return mod(p, s) - s*.5; }
 
 float mindex = 0.;
 vec2 mparam = vec2(0.);
-float dbg = 0.;
-
 const float guard = 4.;
 vec4 rnd, rnd2;
-float cellDist(vec4 cp, vec2 cell) {
-	rnd = noise24(cell);
-	rnd2 = noise24(cell/8.);
-	float height = 3. * (2. + floor(rnd.w * 4.) + 20. * smoothstep(.8,.99, rnd.w) * smoothstep(.6, .9, rnd2.w));
 
-	return min(
-		min(
-			max(vmax3(cp.xyz + rnd.xyz * 4.), cp.w - height),
-			max(vmax3(cp.xyz + rnd.zxw * 7.), cp.w - height - 3. * floor(rnd.w * 4.))),
-		max(vmax3(cp.yzx + 10. * rnd.xyz), cp.w - height + 3. * floor(rnd.y * 2.)));
-}
 
 float world(vec3 p) {
 	vec2 road_normal = normalize(vec2(-1.,2.));
 	float highway = 12. - abs(dot(road_normal,p.xz)),
 		highway_length = dot(vec2(road_normal.y, -road_normal.x), p.xz);
 
+	float r = length(p.xz);
+	mindex = 1.;
+	if (r > 5e3)
+		return p.y - min(1.,(r-5e3)/500.) * 400. * noise24(p.xz/300.).x;
+
+	float d = p.y;
+
 	vec2 cell = floor(p.xz / 30.);
 	p.xz = rep2(p.xz, vec2(30.));
+	rnd = noise24(cell);
+	rnd2 = noise24(cell/8.);
+
 	vec3 cell_border = vec3(abs(p.xz)-15., highway);
+	vec3 cp = vec3(cell_border + guard);
 	float b = min(vmax2(cell_border.xy), -cell_border.z);
-	float cd = cellDist(vec4(cell_border + guard, p.y), cell);
+	float height = 3. * floor(2. + rnd.w * 4. + 20. * smoothstep(.8,.99, rnd.w) * smoothstep(.6, .9, rnd2.w));
+	float cd = min(
+		min(
+			max(vmax3(cp.xyz + rnd.xyz * 4.), p.y - height),
+			max(vmax3(cp.xyz + rnd.zxw * 7.), p.y - height - 3. * floor(rnd.w * 4.))),
+		max(vmax3(cp.yzx + 10. * rnd.xyz), p.y - height + 3. * floor(rnd.y * 2.)));
+
 	mparam =
 		mix(
 			vec2(
@@ -227,14 +226,13 @@ float world(vec3 p) {
 				highway_length)
 		, step(0., cell_border.z));
 
-	float d = p.y;
-	mindex = 1.;
 	if (cd < d) {
 		d = cd;
 		mindex = 0.;
 		mparam = cell_border.xy;
 	}
-	return min(d, -b + guard);
+	//return max(p.y-90., min(d, -b + guard));
+	return max(p.y-90., min(d, -b + guard));
 }
 
 float L;
@@ -244,17 +242,16 @@ void march(float maxL) {
 		P = O + D * L;
 		float dd = world(P);
 		L += dd;
-		if (dd < .0003 * L  || L > maxL) return;
+		if (dd < L / F.x  || L > maxL) return;
 	}
 	L = maxL;
 }
 
-float m_kd = .3;
-float m_shine;
+float m_kd, m_shine;
 float dirlight(vec3 dir) {
 	return mix(
 		max(0., dot(N, dir)) / 3.,
-		max(0., pow(dot(N, normalize(dir - D)), m_shine) * (m_shine + 8.) / 24.),
+		pow(max(0., dot(N, normalize(dir - D))), m_shine) * (m_shine + 8.) / 24.,
 		m_kd
 	);
 }
@@ -264,46 +261,52 @@ float poslight(vec3 pos) {
 }
 
 void main() {
-	//const vec2 res = vec2(640., 360.);//*2.;
-	vec2 uv = gl_FragCoord.xy/vec2(0.000,0.000)* 2. - 1.; uv.x *= vec2(0.000,0.000).x / vec2(0.000,0.000).y;
+	random = noise24(gl_FragCoord.xy + t * 1e4);
 
-	if (gl_FragCoord.y < 10.) {
-		gl_FragColor = vec4(vec3(step(gl_FragCoord.x / vec2(0.000,0.000).x, t / 208.)), 1.);
-		return;
-	}
-
-	//gl_FragColor = noise24(gl_FragCoord.xy);return;
-	//t += noise24(gl_FragCoord.xy + t*100.*vec2(17.,39.)).x;
+	//if (gl_FragCoord.y < 10.) { gl_FragColor = vec4(vec3(step(gl_FragCoord.x / F.x, t / 232.)), 1.); return; }
 
 	vec3 at = vec3(0.);
-	O = vec3(mod(t*2., 10000.) - 5000., 1000. + 500. * sin(t/60.), mod(t*10., 10000.) - 5000.);
+	O = vec3(
+		-300.,
+		200.,
+		mod(t, 64.) * 4.
+		);
 
-	if (t < 64.) {
-		I = 10. * t / 64.;
-		at = vec3(10000., 15000., -10000.);
-	} else if (t < 128.) {
-		O.y = 1000.;
+
+	if (t < 32.) {
+		at = vec3(0., 340., 200.);
+	} else if (t < 64.) {
 		float k = (t - 64.) / 64.;
-		O = vec3(k * 1000., 1500., 3000.);
-		at = O+vec3(-300., 0., -300.);
+		O = vec3(k * 100., 250., -36.);
+		at = O + vec3(-30., 0., -30.);
 		at.y = 10.;
 		sundir.y += .01 * k * k * k;
+	} else if (t < 128.) {
 	} else if (t < 144.) {
-		sundir.y = .01;
+		at = vec3(O.x+9., 90., O.z+20.);
+		O.y = 100.;
+	//	sundir.y = .01;
 	} else {
 		float k = (t - 144.) / 64.;
-		O = vec3(mod(k*2., 10000.) - 500., 1000. - 200. * k, mod(k*10., 10000.) - 5000.);
-		at.y = 4000. * k;
-		//sundir.y = .01 + .5* k;// * k * k * k;
-		sundir.y = .01 + 2.* k;// * k * k * k;
-		//I = 10. + 200. * max(0., (t - 196.) / 16.);
+		//-18, 70, 1
+		at = vec3(500., smoothstep(204., 232., t) * 800., 200.);
+		O = vec3(
+			-400 + k * 100.,
+			230. - k * 10.,
+			-130. - k * 140.
+			);
+		sundir.y = .01 + k * 1.5;// * k * k * k;
 	}
+
+	//I = 10. + 200. * max(0., (t - 208.) / 16.);
+
 	sundir = normalize(sundir);
 	D = normalize(O - at);
 
-	O = vec3(-1.000,4.000,8.000) * 3.; D = -normalize(vec3(0.700,-0.170,0.670));
+	//O = vec3(-1.000,4.000,8.000) * 3.; D = -normalize(vec3(0.700,-0.170,0.670));
+
 	vec3 x = normalize(cross(E.xzx, D));
-	D = mat3(x, normalize(cross(D, x)), D) * normalize(vec3(uv, -2.));
+	D = mat3(x, normalize(cross(D, x)), D) * normalize(vec3((gl_FragCoord.xy + random.xy)/F.xy * 2. - 1., -2.) * vec3(F.x / F.y, 1., 1.));
 
 	vec3 color = vec3(0.);
 
@@ -313,22 +316,16 @@ void main() {
 		N = normalize(vec3(
 			world(P+E.yxx), world(P+E.xyx), world(P+E.xxy)) - world(P));
 
-		//vec3 albedo = vec3(.2, .6, .1);
-		float
-			fx = max(-mparam.x, -mparam.y),
-			fy = mod(P.y, 3.),
-			fxnum = floor(fx / 2.),
-			fynum = floor(P.y / 3.),
-			wfx = mod(fx, 2.),
-			window = step(1., fy) * step(fy, 2.5) * step(.1, wfx) * step(wfx, 1.9);
-		//vec2 wxy = vec2(vmax2(-mparam) / 2., mod(P.y, 3.));
-		//vec2 wnxy = vec2(
-		//float window = step(1., wxy.y);
-		vec3 albedo = mix(vec3(.1) + .02 * rnd.yzw, vec3(.6), window) * (.1 + .9 * smoothstep(0., 24., P.y));
-		m_kd = mix(.3, .8, window);
+		vec2 f = vec2(vmax2(-mparam), P.y),
+			fxy = mod(f, vec2(3.)),
+			fxyC = floor(f / 3.);
+		float window = step(1.3, fxy.y) * step(.8, fxy.x);
+
+		vec3 albedo = mix(vec3(.1) + .02 * rnd.yzw, vec3(.2), window) * (.1 + .9 * smoothstep(0., 24., P.y));
+		m_kd = mix(.3, .9, window);
 		m_shine = mix(18., 1e4, window);
 
-		//N += .02 * (noise24(mparam*4.).xyz - .5);
+		//N += .01 * (noise31(P*4.) - .5);
 
 		if (mindex > 0.) {
 			window = 0.;
@@ -341,6 +338,7 @@ void main() {
 					* step(.5, fract(mparam.y/4.)))
 				),
 				step(1., mparam.x));
+			
 			/*
 			color =
 				100. * vec3(.9, .1, .05)
@@ -352,31 +350,40 @@ void main() {
 
 		color += Lin(P, sundir, escape(P, sundir, Ra)) * I * dirlight(sundir);
 
-		vec4 PP = vec4(floor(P.xz / 30.) * 30., 0., 30.);
-		color += 50. * vec3(.9, .5, .2) * (
-			poslight(vec3(PP.xy, 3.).xzy) +
-			poslight(vec3(PP.xy+PP.zw*rnd.x, 3.).xzy) +
-			poslight(vec3(PP.xy+PP.wz*rnd.y, 3.).xzy) +
-			poslight(vec3(PP.xy+PP.ww*rnd.z, 3.).xzy)
-			);
+		vec2 streetlight_cell = floor((P.xz + 15.) / 30.);
+		//color += noise24(streetlight_cell).xzy;
+		color += 250. * mix(
+				vec3(.9, .5, .2),
+				vec3(.2, .5, .9),
+				rnd.z * .5)
+			* rnd.w
+			* poslight
+				(vec3(30. * (streetlight_cell + (noise24(streetlight_cell).xy - .5)), 3.).xzy)
+			* smoothstep(144., 140., t - rnd.w * 4. - rnd2.w * 8.)
+			;
 
 		vec3 opposite = vec3(-sundir.x, sundir.y, -sundir.z);
 		clouds = false;
-		m_kd = .5;
-		color += scatter(P, opposite, escape(P, opposite, Ra), vec3(0.)) * dirlight(opposite);// * m_kd * max(0., dot(N, opposite));// / 3.;
+		//m_kd = .5;
+		color += scatter(P, opposite, escape(P, opposite, Ra), vec3(0.)) * 6. * dirlight(opposite);// * m_kd * max(0., dot(N, opposite));// / 3.;
 		clouds = true;
 		color = scatter(O, D, L, color * albedo);
 
 		//vec4 wrnd = noise24(floor(wxy) + t);
-		//vec4 wrnd = noise24(floor(vec2(fxnum, fynum)) + t);
-		//vec4 wrnd = noise24(floor(vec2(vmax2(P.xz), P.y)));
-		//color += window * vec3(.3 + .2 * wrnd.xyz) * smoothstep(.7, .9, wrnd.w);
+		vec4 wrnd = noise24(floor(fxyC + P.xz/3.));
+		//vec4 wrnd = noise24(floor(vec2(vmax2(P.xz/2.), P.y)));
+		color += window
+			* (vec3(.2 + .2 * wrnd.xyz)
+				//	+ .3 * noise24(fxy*3.).yzw
+				)
+			* smoothstep(.85, 1., wrnd.w + .1 * smoothstep(.34, .8, .2 * rnd.w + .8 * rnd2.w) 
+					- max(0., t - 136.)/64.)
+			;
 	} else
 		color = scatter(O, D, escape(O, D, Ra), vec3(0.));
 
 	//color = vec3(fract(steps/100.));
 
-	//color = vec3(-N.y);
 	//gl_FragColor = color.x < 0.0001 ? vec4(1.,0.,0.,1.) : vec4(pow(color, vec3(1./2.2)),.5);
-	gl_FragColor = vec4(pow(color, vec3(1./2.2)),.9);
+	gl_FragColor = vec4(pow(smoothstep(0., 32., t) * color /*+ smoothstep(0., 8., t - 208.)*/, vec3(1./2.2)),.3);
 }
