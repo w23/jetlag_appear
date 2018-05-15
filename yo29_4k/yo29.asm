0b87a8defd8148a900f51a1bd08f024454960b43
Ivan Avdeev <me@w23.ru>
2018-05-15 13:01:58 +0700

    yo29: uppdate music

--- yo29_4k/yo29.asm
+++ yo29_4k/yo29.asm
@@ -8,14 +8,13 @@ global _entrypoint
 %define NOISE_SIZE_BYTES (4 * NOISE_SIZE * NOISE_SIZE)
 
 ;%include "4klang.inc"
-%define USE_SECTIONS
 %define SAMPLE_RATE	44100
 %define MAX_INSTRUMENTS	10
 %define MAX_VOICES 2
 %define HLD 1
 %define BPM 60.000000
-%define MAX_PATTERNS 121
-%define PATTERN_SIZE_SHIFT 3
+%define MAX_PATTERNS 57
+%define PATTERN_SIZE_SHIFT 4
 %define PATTERN_SIZE (1 << PATTERN_SIZE_SHIFT)
 %define	MAX_TICKS (MAX_PATTERNS*PATTERN_SIZE)
 %define	SAMPLES_PER_TICK 11025
