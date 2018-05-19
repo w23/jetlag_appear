b30cf4a86348d77fa5b9857c5d6270c6ceae03c9
Ivan Avdeev <me@w23.ru>
2018-05-20 02:01:29 +0700

    fix build and projects on windows

--- run-tool.bat
+++ run-tool.bat
@@ -1,1 +1,2 @@
-Release\tool.exe -w 1280 -h 720
+cd yo29_4k
+..\Release\tool.exe -w 1280 -h 720
--- run-tool.sh
+++
@@ -1,1 +0,0 @@
-clang++ -g -O0 --std=c++11 -Iatto atto/src/app_x11.c atto/src/app_linux.c tool.cc -o tool -lGL -lm -lX11 -lXfixes -o tool && ./tool trace.glsl post.glsl
